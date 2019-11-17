b90ffa00b82b10b8bc9c4e6e7121cc7c2aeb923e
yinshunming <yin-shun-ming@163.com>
2019-11-17 19:46:37 +0800

    init commit

---
+++ src/main/java/com/nju/spider/crawler/GartnerReportCrawler.java
@@ -0,0 +1,10 @@
+package com.nju.spider.crawler;
+
+public class GartnerReportCrawler {
+    private static final String url = "https://www.gartner.com/en/products/special-reports";
+
+    public void crawl() {
+
+    }
+
+}
---
+++ src/main/java/com/nju/spider/utils/HttpUtils.java
@@ -0,0 +1,53 @@
+package com.nju.spider.utils;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class HttpUtils {
+
+    public static String doGet(String url) {
+        String resStr = null;
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet httpGet = new HttpGet("http://localhost:12345/doGetControllerOne");
+
+        CloseableHttpResponse response = null;
+        try {
+            response = httpClient.execute(httpGet);
+            // 从响应模型中获取响应实体
+            HttpEntity responseEntity = response.getEntity();
+            System.out.println("响应状态为:" + response.getStatusLine());
+            if (responseEntity != null) {
+                resStr = EntityUtils.toString(responseEntity);
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // 释放资源
+                if (httpClient != null) {
+                    httpClient.close();
+                }
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return resStr;
+    }
+}
