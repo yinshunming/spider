2347b849afe650316dd97905a222a0020f20a86d
yinshunming <yin-shun-ming@163.com>
2019-12-02 21:30:26 +0800

    德勤中国爬虫基本完成，待正式抓取调整

--- src/main/java/com/nju/spider/crawler/DeloitteCnCrawler.java
+++ src/main/java/com/nju/spider/crawler/DeloitteCnCrawler.java
@@ -1,5 +1,7 @@
 package com.nju.spider.crawler;
 
+import com.nju.spider.bean.Report;
+import com.nju.spider.db.ReportDaoUtils;
 import com.nju.spider.utils.FormatUtils;
 import com.nju.spider.utils.HttpUtils;
 import com.nju.spider.utils.MyHtmlCleaner;
@@ -7,8 +9,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.htmlcleaner.TagNode;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 德勤中国网站爬虫
@@ -21,13 +27,15 @@ public class DeloitteCnCrawler extends BaseCrawler{
 
     private static final long intervalTime = 8 * 3600 * 1000;  //8h间隔抓取时间
 
-    private static final String historyIndexUrl = "https://www2.deloitte.com/cn/zh/footerlinks/pressreleasespage.html?i=1;page=%s;" +
-            "q=*;rank=rank-search;sp_q_18=%22%E6%96%B0%E9%97%BB%E7%A8%BF%22;sp_s=date-published%7Ctitle;sp_x_18=content-type;view=xml&pageNumber=2";
+    private static final String historyIndexUrl = "https://www2.deloitte.com/cn/zh/footerlinks/pressreleasespage.html?i=1;" +
+            "q=*;rank=rank-search;sp_q_18=%22%E6%96%B0%E9%97%BB%E7%A8%BF%22;sp_s=date-published%7Ctitle;sp_x_18=content-type;view=xml&pageNumber=2;page=";
 
-    private static final String history2IndexUrl = "https://www2.deloitte.com/cn/zh/footerlinks/pressreleasespage.html?i=1;page=%s;" +
-            "pageNumber=2;q=*;rank=rank-search;sp_q_18=%22%E6%96%87%E7%AB%A0%22;sp_s=date-published%7Ctitle;sp_x_18=content-type;view=xml&pageNumber=3";
+    private static final String history2IndexUrl = "https://www2.deloitte.com/cn/zh/footerlinks/pressreleasespage.html?i=1;" +
+            "pageNumber=2;q=*;rank=rank-search;sp_q_18=%22%E6%96%87%E7%AB%A0%22;sp_s=date-published%7Ctitle;sp_x_18=content-type;view=xml&pageNumber=3;page=";
 
     private static final String baseUrl = "https://www2.deloitte.com";
+
+    private static final Pattern titlePattern = Pattern.compile("《(.*)》");
 
     private static ThreadLocal<SimpleDateFormat> publishDateFormatThreadLocal = ThreadLocal.withInitial(() -> new SimpleDateFormat("dd MMM yyyy", Locale.SIMPLIFIED_CHINESE));
 
@@ -52,35 +60,71 @@ public class DeloitteCnCrawler extends BaseCrawler{
 
         for (int i = 1; i <= 41; i++) {
 
-            String historyUrlToCrawl = String.format(historyIndexUrl, i);
+            String historyUrlToCrawl = historyIndexUrl + i;
             try {
                 log.info("starting to crawl url: " + historyUrlToCrawl);
                 String res = HttpUtils.doGetWithRetryUsingProxy(historyUrlToCrawl, retryTimes);
                 TagNode rootNode = MyHtmlCleaner.clean(res);
 //                Object [] articleUrls = rootNode.evaluateXPath("//ul//li[@class='press-release']//h2//a//@href");
                 Object [] articleElements = rootNode.evaluateXPath("//div[@class='release-text-container']");
+                List<Report> reportList = new ArrayList<>();
                 for (Object articleEOb : articleElements) {
                     try {
                         TagNode articleTagNode = (TagNode) articleEOb;
-                        Object[] articleUrlObs = articleTagNode.evaluateXPath("//h2//a//@href");
+                        Object[] articleUrlObs = articleTagNode.evaluateXPath("//h2//a/@href");
                         String articleUrl = baseUrl + articleUrlObs[0];
                         Object[] publishDateObs = articleTagNode.evaluateXPath("//p[@class='release-date']/text()");
                         Date publishDate = null;
                         if (publishDateObs != null && publishDateObs.length > 0) {
-                            String publishDateStr = ((String) publishDateObs[0]).trim();
+                            String publishDateStr = publishDateObs[0].toString().trim();
                             publishDate = FormatUtils.parseDateByDateFormate(publishDateStr, publishDateFormatThreadLocal.get());
                         }
 
                         String res2 = HttpUtils.doGetWithRetryUsingProxy(articleUrl, retryTimes);
-                        TagNode articleNode = MyHtmlCleaner.clean(res);
-                        String downloadXpath = "//div[@class='downloadpromo section']//a/@href";
-                        String title1 = "//div[@class='downloadpromo section']//button/text()";
+                        TagNode articleNode = MyHtmlCleaner.clean(res2);
+                        Object [] downloadHrefs = articleNode.evaluateXPath("//div[@class='downloadpromo section']//a/@href");
 
-                        String title2 = "//div[@class='contentpagecolctrl section']//a/text()"; //有可能误判
+                        if (downloadHrefs.length > 0) {
+                            String url = baseUrl + downloadHrefs[0];
+
+                            String title = null;
+                            Object[] title1Objs = articleNode.evaluateXPath("//div[@class='downloadpromo section']//button/text()");  //结合正则表达式
+
+                            if (title1Objs.length > 0) {
+                                String title1All = title1Objs[0].toString();
+                                Matcher matcher1 = titlePattern.matcher(title1All);
+                                if (matcher1.find()) {
+                                    title = matcher1.group(1);
+                                }
+                            }
+
+                            //第二种方式，直接在正文中找到第一个书名号的文字，作为title，可能误判
+                            if (title == null) {
+                                Object[] title2Objs = articleNode.evaluateXPath("//div[@class='contentpagecolctrl section']//a/text()");
+                                if (title2Objs.length > 0) {
+                                    String title2All = title1Objs[0].toString();
+                                    Matcher matcher2 = titlePattern.matcher(title2All);
+                                    if (matcher2.find()) {
+                                        title = matcher2.group(1);
+                                    }
+                                }
+                            }
+
+                            Report report = new Report();
+                            report.setPublishTime(publishDate);
+                            report.setUrl(url);
+                            report.setArticleUrl(articleUrl);
+                            report.setIndexUrl(historyUrlToCrawl);
+                            report.setOrgName(orgName);
+                            report.setTitle(title);
+                            reportList.add(report);
+                        }
                     } catch (Exception ex) {
-                        log.error("deaing article elements encounts errpr", ex);
+                        log.error("dealing article elements encounts errpr", ex);
                     }
                 }
+
+                ReportDaoUtils.insertReports(reportList);
             } catch (Exception ex) {
                 log.error("crawling " + historyUrlToCrawl + " encounts error", ex);
             }
