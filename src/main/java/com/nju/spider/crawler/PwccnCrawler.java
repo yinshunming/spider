400fd4fd999a5def6a52b5d7e8ff616ac8087054
yinshunming <yin-shun-ming@163.com>
2019-12-02 21:56:30 +0800

    普华永道中国刚开始工作

--- src/main/java/com/nju/spider/crawler/PwccnCrawler.java
+++ src/main/java/com/nju/spider/crawler/PwccnCrawler.java
@@ -5,9 +5,16 @@ package com.nju.spider.crawler;
  * 普华永道中国爬虫
  */
 public class PwccnCrawler extends BaseCrawler{
+    private static final String orgName = "pwccn";
+    private static final int retryTimes = 5;
+    private static final long intervalTime = 6 * 3600 * 1000;  //6h间隔抓取时间
+
+    //一级页，获取行业名称
+    private static final String firstIndex = "https://www.pwccn.com/zh/industries.html";
+
     @Override
     public String getCrawlName() {
-        return null;
+        return orgName;
     }
 
     @Override
