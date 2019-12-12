675200513b0174b0b509641be6c34e4f8d2fb816
yinshunming <yin-shun-ming@163.com>
2019-12-12 21:14:26 +0800

    nielsen完成60%，待测试+实际跑

--- src/main/java/com/nju/spider/crawler/NielsenCrawler.java
+++ src/main/java/com/nju/spider/crawler/NielsenCrawler.java
@@ -1,5 +1,20 @@
 package com.nju.spider.crawler;
 
+import com.nju.spider.utils.FormatUtils;
+import com.nju.spider.utils.HttpUtils;
+import com.nju.spider.utils.MyHtmlCleaner;
+import com.nju.spider.utils.XpathUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.htmlcleaner.TagNode;
+import org.htmlcleaner.XPatherException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 /**
  * @ClassName NielsenCrawler
  * @Description nielsen网站爬虫
@@ -7,29 +22,89 @@ package com.nju.spider.crawler;
  * @Date 2019/12/10 21:45
  * @Version 1.0
  */
+@Slf4j
 public class NielsenCrawler extends BaseCrawler{
 
-    private static final String xpath = "//iframe[@class='embed-iframe']";
+    private static final String xpath = "//iframe[@class='embed-iframe']/@src";
 
     private static final String xpath2 = "//div[@id='download_links']//a/@href";
 
-    private static final String usaUrl = "https://www.nielsen.com/us/en/insights/report/";
+    private static final String usaUrl = "https://www.nielsen.com/us/en/insights/report/page/%s/";
 
-    private static final String cnUrl = "https://www.nielsen.com/cn/zh/insights/report/";
+    private static final String usaUrl2 = "https://www.nielsen.com/us/en/insights/case-study/%s/";
+
+    private static final String uasUrl3 = "https://www.nielsen.com/us/en/insights/resource/%s/";
+
+    private static final String cnUrl = "https://www.nielsen.com/cn/zh/insights/report/page/%s/";
+
+
+    private static final String orgName = "Nielsen";
+
+    private static final long intervalTime = 8 * 3600 * 1000;  //8h间隔抓取时间
+
+
+    private static ThreadLocal<SimpleDateFormat> publishDateFormatThreadLocal = ThreadLocal.withInitial(() -> new SimpleDateFormat("MM-dd-yyyy", Locale.US));
+
 
 
     @Override
     public String getCrawlName() {
-        return null;
+        return orgName;
     }
 
     @Override
     public long getIntervalTime() {
-        return 0;
+        return intervalTime;
     }
 
     @Override
     public void crawl() {
 
+        for (int i = 1; i <=54; i++ ) {
+            String historyUrlToCrawl = String.format(usaUrl, i);
+            try {
+                String res = HttpUtils.doGetWithRetryUsingProxy(usaUrl);
+                TagNode indexRootTagNode = MyHtmlCleaner.clean(res);
+                Object[] articleIndexObs = indexRootTagNode.evaluateXPath("//article//div[@class='row']//div[@class='col']");
+                for (Object articleIndexOb : articleIndexObs) {
+                    TagNode articleIndexTagNode = (TagNode) articleIndexOb;
+                    String industryName = XpathUtils.getStringFromXpath(articleIndexTagNode, "//article//div[@class='row']//div[@class='col']//header//span/a/@href");
+                    String publishDateStr = XpathUtils.getStringFromXpath(articleIndexTagNode, "//article//div[@class='row']//div[@class='col']//header//time/text()");
+                    Date publishDate = FormatUtils.parseDateByDateFormate(publishDateStr, publishDateFormatThreadLocal.get());
+                    String title = XpathUtils.getStringFromXpath(articleIndexTagNode, "//article//div[@class='row']//div[@class='col']//h2//a/text()");
+                    String articleUrl = XpathUtils.getStringFromXpath(articleIndexTagNode, "//article//div[@class='row']//div[@class='col']//h2//a/@href");
+                    String res2 = HttpUtils.doGetWithRetry(articleUrl);
+                    String pdfUrl = XpathUtils.getStringFromXpath(res2, "//div[@id='download_links']//a/@href");
+                    //此处没找到pdf的链接，则找寻另一种格式
+                    if (StringUtils.isBlank(pdfUrl)) {
+                        String tmpHref = XpathUtils.getStringFromXpath(res2, "//iframe[@class='embed-iframe']/@src");
+                        if (StringUtils.isNotBlank(tmpHref)) {
+                           String res3 = HttpUtils.doPostWithRetryUsingProxy(tmpHref, getPostContent(), getHeaders());
+                           pdfUrl = XpathUtils.getStringFromXpath(res3, "//div[@id='download_links']//a/@href");
+                        }
+                    }
+
+                    if (StringUtils.isNotBlank(pdfUrl)) {
+                        //TODO 填充report
+                    }
+                }
+            } catch (Exception ex) {
+                log.error("error getting article indexs of url " + historyUrlToCrawl, ex);
+            }
+        }
+    }
+
+    private static Map<String, String> getHeaders() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36");
+        //Cookie目前有效期比较长，暂时不用动态的获取
+        headers.put("Cookie", "visitor_id31782=524498679; visitor_id31782-hash=6b0e8b761b73fab11c94d768c4c9329f5b9cd790662ac13eec534c23bce96f53a4bfc9977c8aba513a4312f16aef1435399fd0f8; pardot=t0lmaqm3b05p1od3ts2dkiefld");
+        headers.put("Content-Type", "application/x-www-form-urlencoded");
+        return headers;
+    }
+
+    private static String getPostContent() {
+        //目前也比较固定
+        return "31782_219811pi_31782_219811=SDDF&31782_219813pi_31782_219813=dafafsd&31782_219815pi_31782_219815=12afd%40xds.com&31782_219817pi_31782_219817=dsfa&31782_219819pi_31782_219819=sadffdas&31782_219821pi_31782_219821=2278517&pi_extra_field=&_utf8=%E2%98%83&hiddenDependentFields=";
     }
 }
--- src/main/java/com/nju/spider/utils/HttpUtils.java
+++ src/main/java/com/nju/spider/utils/HttpUtils.java
@@ -7,10 +7,14 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.http.*;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.File;
@@ -18,12 +22,17 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Slf4j
 public class HttpUtils {
     private static final String defaulUserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36";
+
+    private static final String postDefaultContentType = "application/x-www-form-urlencoded";
 
     private static final Pattern fileNamePattern = Pattern.compile("filename=\"(.*)\"");
 
@@ -211,6 +220,8 @@ public class HttpUtils {
         return resStr;
     }
 
+
+
     public static String doGet(String url) {
         return doGet(url, false);
     }
@@ -255,6 +266,7 @@ public class HttpUtils {
         }
         return resStr;
     }
+
 
     public static boolean doDownload(Report report) {
         return doDownload(report, false);
@@ -319,10 +331,140 @@ public class HttpUtils {
         return false;
     }
 
+    public static String doPostWithRetryUsingProxy(String url, String strEntity) {
+        return doPostWithRetry(url, strEntity, null, defaultRetryTimes, true);
+    }
+
+    public static String doPostWithRetryUsingProxy(String url, String strEntity, int retryTimes) {
+        return doPostWithRetry(url, strEntity, null, retryTimes, true);
+    }
+
+    public static String doPostWithRetryUsingProxy(String url, String strEntity, Map<String, String> headersMap) {
+        return doPostWithRetry(url, strEntity, headersMap, defaultRetryTimes, true);
+    }
+
+    public static String doPostWithRetryUsingProxy(String url, String strEntity, Map<String, String> headersMap, int retryTimes) {
+        return doPostWithRetry(url, strEntity, headersMap, retryTimes, true);
+    }
+
+    public static String doPostWithRetry(String url, String strEntity) {
+        return doPostWithRetry(url, strEntity, null, defaultRetryTimes);
+    }
+
+    public static String doPostWithRetry(String url, String strEntity, int retryTimes) {
+        return doPostWithRetry(url, strEntity, null, retryTimes);
+    }
+
+
+    public static String doPostWithRetry(String url, String strEntity, Map<String, String> headersMap) {
+        return doPostWithRetry(url, strEntity, headersMap, defaultRetryTimes);
+    }
+
+
+    public static String doPostWithRetry(String url, String strEntity, Map<String, String> headersMap, int retryTimes) {
+        return doPostWithRetry(url, strEntity, headersMap, retryTimes, false);
+    }
+
+
+    public static String doPostWithRetry(String url, String strEntity, Map<String, String> headersMap, int retryTimes, boolean usingProxy) {
+        for (int i = 0 ; i < retryTimes; i++) {
+            String res = doPost(url, strEntity, headersMap, usingProxy);
+            if (StringUtils.isNotBlank(res)) {
+                return res;
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+            }
+        }
+
+        return null;
+    }
+
+
+    public static String doPost(String url, String strEntity, Map<String, String> headersMap) {
+        return doPost(url, strEntity, headersMap, false);
+    }
+
+
+    public static String doPost(String url, String strEntity, Map<String, String> headersMap, boolean usingProxy) {
+        String resStr = null;
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPost httpPost = new HttpPost(url);
+
+        RequestConfig requestConfig =  RequestConfig.custom().setConnectTimeout(5000).setSocketTimeout(10000).build();
+        if (usingProxy) {
+            HttpHost proxy = getProxy();
+            requestConfig = RequestConfig.custom().setConnectTimeout(5000).setSocketTimeout(15000).setProxy(proxy).build();
+        }
+
+        CloseableHttpResponse response = null;
+        try {
+            httpPost.setConfig(requestConfig);
+            httpPost.setHeader("User-Agent", defaulUserAgent);
+            httpPost.setHeader("Content-Type", postDefaultContentType);
+
+            if (headersMap != null && headersMap.size() > 0) {
+                for (Map.Entry<String, String> entry : headersMap.entrySet()) {
+                    httpPost.setHeader(entry.getKey(), entry.getValue());
+                }
+            }
+
+            if (StringUtils.isNotBlank(strEntity)) {
+                httpPost.setEntity(new StringEntity(strEntity));
+            }
+
+            response = httpClient.execute(httpPost);
+            HttpEntity responseEntity = response.getEntity();
+
+            if (responseEntity != null && HttpStatus.SC_OK == response.getStatusLine().getStatusCode()) {
+                resStr = EntityUtils.toString(responseEntity);
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (httpClient != null) {
+                    httpClient.close();
+                }
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+        return resStr;
+    }
+
     public static void main(String [] args) {
-        String filePath = "D:/test/123.pdf";
-        String fileName = "rra.pdf";
-        String ret = filePath.replaceAll("[^/]+\\.pdf", fileName);
-        System.out.println(ret);
+//        String filePath = "D:/test/123.pdf";
+//        String fileName = "rra.pdf";
+//        String ret = filePath.replaceAll("[^/]+\\.pdf", fileName);
+//        System.out.println(ret);
+//        List<NameValuePair> params = new ArrayList<NameValuePair>();
+//        NameValuePair pair = new BasicNameValuePair("31782_219811pi_31782_219811", "SDDF");
+//        NameValuePair pair2 = new BasicNameValuePair("31782_219813pi_31782_219813", "dafafsd");
+//        NameValuePair pair3 = new BasicNameValuePair("31782_219815pi_31782_219815", "12afd@xds.com");
+//        NameValuePair pair4 = new BasicNameValuePair("31782_219817pi_31782_219817", "dsfa");
+//        NameValuePair pair5 = new BasicNameValuePair("31782_219821pi_31782_219821", "2278517");
+//        NameValuePair pair6 = new BasicNameValuePair("pi_extra_field", "");
+//        NameValuePair pair7 = new BasicNameValuePair("_utf8", "%E2%98%83");
+//        NameValuePair pair8 = new BasicNameValuePair("hiddenDependentFields", "");
+//        params.add(pair);
+//        params.add(pair2);
+//        params.add(pair3);
+//        params.add(pair4);
+//        params.add(pair5);
+//        params.add(pair6);
+//        params.add(pair7);
+//        params.add(pair8);
+
+        String res = HttpUtils.doPostWithRetry("https://www4.nielsen.com/l/31782/2019-09-20/jdwfvn", "31782_219811pi_31782_219811=SDDF&31782_219813pi_31782_219813=dafafsd&31782_219815pi_31782_219815=12afd%40xds.com&31782_219817pi_31782_219817=dsfa&31782_219819pi_31782_219819=sadffdas&31782_219821pi_31782_219821=2278517&pi_extra_field=&_utf8=%E2%98%83&hiddenDependentFields=");
+        System.out.println(res);
     }
 }
